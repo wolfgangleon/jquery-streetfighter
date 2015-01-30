01ed247852e309f37db0869a60727d71a65ae6b1
wolfgangleon <wolfgangrleonh@gmail.com>
2015-01-30 13:47:07 -0500

    script

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
-	
+	$('.ryu').mouseenter(function() {
+    	$('.ryu-still').hide();
+    	$('.ryu-ready').show();
+  	})
+  	.mouseleave(function() {
+    	$('.ryu-still').show();
+    	$('.ryu-ready').hide();
+ 	 })
+
+  	.mousedown(function(){
+  		$('.ryu-ready').hide();
+  		$('.ryu-throwing').show();
+  		$('.hadouken').show();
+  	})
+
+	.mouseup(function() {
+  		$('.ryu-throwing').hide();
+  		$('.ryu-ready').show();
+	});
+
 });
